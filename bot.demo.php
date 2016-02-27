df3770b791fe293e9787675c89077e362173316f
Whitebock <whitebock@web.de>
2016-02-27 23:16:27 +0100

    Add demo

---
+++ bot.demo.php
@@ -0,0 +1,27 @@
+<?php
+	require_once('./TelegramBot/api.telegram.php');
+	$bot = new Bot('token');
+
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		
+		$me = $bot -> getMe();
+		echo 'ID: '.$me -> id.'<br>';
+		echo 'Username: '.$me -> username.'<br>';
+		echo 'Full Name: '.$me -> first_name.'<br>';
+
+		//$obj = $bot -> setWebhook(array('url' => 'https://example.com/bot.demo.php'));
+		//$boolstring = ($obj) ? 'true' : 'false';
+		//echo 'Webhook set: '.$boolstring;
+	}
+	else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+		$stream_data = file_get_contents('php://input');
+		$json = json_decode($stream_data);
+		$update = parseClass($json, 'Update');
+		
+		$userid = $update -> message -> chat -> id;
+		
+		$bot -> sendMessage($userid, 'DEMO');
+	}
+
+?>
